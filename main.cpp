7d32176d0f48280b43a5b29f1342ceee92537675
CiuffarellaP <117938895+CiuffarellaP@users.noreply.github.com>
2022-12-20 15:37:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
-using namespace std;
+#include "math.h"
 
+float f (float a){
+    return (a*a*cos(a)+1);
+}
+
+using namespace std;
 int main() {
-   return 0;
+    float a,b,x,err;
+    do{
+        cout<<"inserire estremi: ";
+        cin>>a>>b;
+    }while (f(a)*f(b)>=0);
+   do{
+       x = (a+b)/2;
+       if (f(x)==0){
+           cout<<x<<endl<<f(x);
+       }else {
+           if (f(a) * f(b) < 0) {
+               b = x;
+           } else {
+               a = x;
+           }
+           err = abs((b - a) / 2);
+       }
+   }while (err>=1e-6);
+    cout<<x<<endl<<f(x);
+    return 0;
 }
